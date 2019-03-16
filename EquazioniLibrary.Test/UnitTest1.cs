820cd9b1aae4850cd1875e2d29212f4cc17cad70
Stefan Milosevic <smilosevic86@outlook.it>
2019-03-16 15:54:06 +0100

    Creati due test utilizzando il data row per verificare il metodo creato in precedenza

--- EquazioniLibrary.Test/UnitTest1.cs
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -6,6 +6,27 @@ namespace EquazioniLibrary.Test
     [TestClass]
     public class UnitTest1
     {
-        
+        [DataTestMethod]
+        [DataRow(5)]
+        [DataRow(-5)]
+        public void IsDeterminedTest(double a)
+        {
+            bool risposta_attesa = true;
+
+            bool risposta = EquazioniLibrary.Equazioni.IsDetermined(a);
+
+            Assert.AreEqual(risposta_attesa, risposta);
+        }
+
+        [TestMethod]
+        public void IsDeterminedTest2()
+        {
+            double a = 0;
+            bool risposta_attesa = false;
+
+            bool risposta = EquazioniLibrary.Equazioni.IsDetermined(a);
+
+            Assert.AreEqual(risposta_attesa, risposta);
+        }
     }
 }
